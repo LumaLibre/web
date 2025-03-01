469845d898fcc896c197f0a1a9aea7b00a1d449c
Jsinco <jonah@jsinco.dev>
2025-03-01 15:06:49 -0500

    tasks and github linguist stuff

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    java
+    kotlin("jvm") version "2.0.21"
+    id("com.gradleup.shadow") version "8.3.5"
+    //id("dev.jsinco.pterodactyldeploy") version "1.2-SNAPSHOT"
+}
+
+tasks {
+    build {
+
+    }
+}
